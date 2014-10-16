c99c5fb619fbcddbcee30aef3049a27dec24065a
robert-fiedor <robert.fiedor@kaplan.com>
2014-10-16 16:26:13 -0400

    nodemon, api stuff

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+
+http://scotch.io/bar-talk/setting-up-a-mean-stack-single-page-application
+
+nodemon server.js
+
 # MEAN Stack Single Page Application Starter
 
 This is a repo for a starter appliation for a Single Page MEAN Stack application. Just download and install and you have a good foundation for building application. 
--- app/routes.js
+++ app/routes.js
@@ -4,9 +4,27 @@ module.exports = function(app) {
 	// handle things like api calls
 	// authentication routes
 
+    app.get('/api', function(req, res) {
+        // use mongoose to get all nerds in the database
+//        Nerd.find(function(err, nerds) {
+//
+//            // if there is an error retrieving, send the error.
+//            // nothing after res.send(err) will execute
+//            if (err)
+//                res.send(err);
+//
+//            res.json(nerds); // return all nerds in JSON format
+//        });
+
+        res.json({d:1}); // return all nerds in JSON format
+
+
+
+    });
+
 	// frontend routes =========================================================
 	// route to handle all angular requests
-	app.get('*', function(req, res) {
+	app.get('/index', function(req, res) {
 		res.sendfile('./public/index.html');
 	});
 
